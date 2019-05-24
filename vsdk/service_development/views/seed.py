6fe17708270b0b9f0bd7328776e6ecfb5b1a5d5e
Kim <ke.vanputten@gmail.com>
2019-05-24 19:14:13 +0200

    retrace steps: view/seed

--- vsdk/service_development/views/seed.py
+++ vsdk/service_development/views/seed.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
-from django.urls import reverse
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from ..models import Seed, CallSession
 
 class Seed(TemplateView):
-    def registration_process(self, request, session):
-        redirect_url = reverse('service-development:user-registration', args =[session.id])
-        return
 
     def get(self, request, session_id):
         session = get_object_or_404(CallSession, pk=session_id)
